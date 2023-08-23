36d337e4b4a1298588d12061cc0cbd510e5f1754
Junaeid <junaeid.md48@gmail.com>
2023-08-23 18:57:55 +0600

    Configuring mysql and express for API request

--- server/index.js
+++ server/index.js
@@ -10,8 +10,44 @@ const db = mysql.createConnection({
     database: "bookStore"
 })
 
+// Setting up express to accept client files.
+app.use(express.json());
+
+if(db) console.log("Connected");
+
 app.get("/", (req, res) =>{
-    res.json("Hello this is from backend server.")
+    res.json("Hello this is from backend server.");
+})
+
+// Getting data from the database.
+app.get("/books", (req, res) =>{
+    const query = "SELECT * FROM books;";
+    db.query(query, (err, data) =>{
+        if(err){
+            return res.json(err);
+        }else{
+            return res.json(data);
+        }
+    })
+})
+
+
+//Sending data to the Database
+
+app.post("/books", (req, res) =>{
+    const query = "INSERT INTO books (`booktitle`, `bookDesc`, `cover`) VALUES (?);";
+    const values = [
+        req.body.bookTitle,
+        req.body.bookDesc,
+        req.body.cover
+    ];
+    db.query(query,[values], (error, data) =>{
+        if(error){
+            return res.json(error);
+        }else{
+            return res.json("New book inserted successfully!!");
+        }
+    })
 })
 
 app.listen(8001, ()=>{
