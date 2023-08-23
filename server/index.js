5631a7c5b466bcf2729d539f72abde7c3a79bbc6
Junaeid <junaeid.md48@gmail.com>
2023-08-24 03:34:06 +0600

    Adding API request for single book info and updating book info.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
-import mysql from "mysql"
-
+import mysql from "mysql";
+import cors from "cors";
 
 const app = express();
 const db = mysql.createConnection({
@@ -12,6 +12,10 @@ const db = mysql.createConnection({
 
 // Setting up express to accept client files.
 app.use(express.json());
+
+// setting up cors for passing api request to the axios.
+app.use(cors())
+
 
 if(db) console.log("Connected");
 
@@ -31,21 +35,67 @@ app.get("/books", (req, res) =>{
     })
 })
 
+// geting a single book data via id.
+app.get("/books/:id", (req, res) =>{
+    const id = req.params.id;
+    const query = "SELECT * FROM books WHERE id = ?;";
+    db.query(query,[id], (err, data) =>{
+        if(err){
+            return res.json(err);
+        }else{
+            return res.json(data);
+        }
+    })
+})
+
 
 //Sending data to the Database
 
 app.post("/books", (req, res) =>{
-    const query = "INSERT INTO books (`booktitle`, `bookDesc`, `cover`) VALUES (?);";
+    const query = "INSERT INTO books (`booktitle`, `bookDesc`, `cover`, `price`) VALUES (?);";
     const values = [
         req.body.bookTitle,
         req.body.bookDesc,
-        req.body.cover
+        req.body.cover,
+        req.body.price
     ];
     db.query(query,[values], (error, data) =>{
         if(error){
             return res.json(error);
         }else{
             return res.json("New book inserted successfully!!");
+        }
+    })
+})
+
+// delete a book
+app.delete("/books/:id", (req, res)=>{
+    const id = req.params.id;
+    const query = "DELETE FROM books WHERE id = ?"
+    db.query(query,[id], (error, data)=>{
+        if (error) {
+            return res.json(error);
+        } else {
+            return res.json("Book removed successfully!!!!");
+        }
+    })
+})
+
+// update book data
+app.put("/books/:id", (req, res)=>{
+    const id = req.params.id;
+    const query = "UPDATE books set `bookTitle` = ?, `bookDesc` = ?, `cover` = ?, `price` = ? WHERE id = ?"
+    const values = [
+        req.body.bookTitle,
+        req.body.bookDesc,
+        req.body.cover,
+        req.body.price
+    ]
+    db.query(query,[...values,id], (error, data)=>{
+        if (error) {
+            return res.json(error);
+        } else {
+            return res.json("Book updated successfully!!!!");
         }
     })
 })
